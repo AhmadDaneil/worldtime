1e0d1c74121af9af6d44bafc6f25600222f11920
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-03-15 16:55:12 +0800

    World Time API & Creating a WorldTime Class

---
+++ lib/services/world_time.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+  String? location; //Location name for the UI
+  String time = ""; //Time in that location
+  String? flag; //URL to asset flag
+  String? url; //Location URL for api endpoint
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async
+  {
+    String apiUrl = 'https://timeapi.io/api/time/current/zone?timeZone=Europe%2FLondon';
+    //make the request
+    http.Response response = await http.get(Uri.parse(apiUrl));
+    
+    Map data = jsonDecode(response.body);
+    if(response.statusCode == 200 && response.body.isNotEmpty) {
+    json.decode(response.body);
+}
+    //print(data);
+
+    //get properties from data
+    String datetime = data['dateTime'];
+    String timezone = data['timeZone'];
+    //print(time);
+    print(timezone);
+
+    
+    time = datetime.toString();
+
+
+    
+  }
+
+}
+
