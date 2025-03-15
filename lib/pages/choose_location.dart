c55722b8c951c6450e19241157fee417ab2c31e2
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-03-15 12:50:27 +0800

    Flutter packages

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,28 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async
-  {
-    //Simulate network request for username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Yoshi';
-    });
-
-    //Simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'Vega, musician & egg collector';
-    });
-
-    print('$username - $bio');
-
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('Hey there');
-    }
 
   @override
   Widget build(BuildContext context) {
