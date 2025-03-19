40a6c06e6795fcaebba80cf2f14f941f6f98e202
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-03-19 16:00:18 +0800

    ListView Builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -8,6 +9,15 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(location: 'Berlin', flag: 'german.png', url: 'zone?timeZone=Europe%2FBerlin'),
+    WorldTime(location: 'Cairo', flag: 'egypt.png', url: 'zone?timeZone=Africa%2FCairo'),
+    WorldTime(location: 'Seoul', flag: 'korea.png', url: 'zone?timeZone=Asia%2FSeoul'),
+    WorldTime(location: 'New York', flag: 'united_states.png', url: 'zone?timeZone=America%2FNew_York'),
+    WorldTime(location: 'Canberra', flag: 'australia.png', url: 'zone?timeZone=Australia%2FCanberra'),
+    WorldTime(location: 'Kuala Lumpur', flag: 'malaysia.png', url: 'zone?timeZone=Asia%2FKuala_Lumpur'),
+  ];
 
 
   @override
@@ -22,6 +32,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical:1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location!),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        }
+      )
       
     );
   }
